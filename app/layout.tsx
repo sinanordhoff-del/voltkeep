39fcedd78af330317d98e35189f6a20b8ae5f211
Sina Nordhoff <sina.nordhoff@gmail.com>
2026-08-02 22:40:14 +0200

    Add Google Fonts preconnect and stylesheets

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;500;600;700;800&family=IBM+Plex+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
